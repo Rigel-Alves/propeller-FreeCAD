65c62677498be1d42ce9b64aabf1710e0b046afa
Rigel-Alves <35175087+Rigel-Alves@users.noreply.github.com>
2025-04-15 14:20:05 +0200

    initial commit

---
+++ script.py
@@ -0,0 +1,232 @@
+import sys
+import numpy as np
+
+# NACA 2412
+m = 0.02
+p = 0.4
+t = 0.12
+
+def get_yt(x, t):
+    return 5*t*(0.2969*x**0.5 - 0.1260*x - 0.3516*x**2 + 0.2843*x**3 - 0.1015*x**4)
+
+def get_yc(x, m, p):
+    return np.where((x >= 0) & (x < p), m/p**2 * (2*p*x - x**2), m/(1-p)**2 * ((1 - 2*p) + 2*p*x - x**2))
+    # if (x >= 0) and (x < p):
+    #     return m/p**2 * (2*p*x - x**2)
+    # elif (x >= p) and (x <= 1):
+    #     return m/(1-p)**2 * ((1 - 2*p) + 2*p*x - x**2)
+    # else:
+    #     print("invalid x")
+    #     sys.exit()
+
+def get_dycdx(x, m, p):
+    return np.where((x >= 0) & (x < p), 2*m/p**2 * (p - x), 2*m/(1 - p)**2 * (p - x))
+    # if x >= 0 and x < p:
+    #     return 2*m/p**2*(p-x)
+    # elif x >= p and x <= 1:
+    #     return 2*m/(1-p)**2*(p-x)
+    # else:
+    #     print("invalid x")
+    #     sys.exit()
+
+def get_theta(x, m, p):
+    dycdx = get_dycdx(x, m, p)
+    return np.arctan(dycdx)
+
+def get_coords(x, m, p, t):
+    yt = get_yt(x, t)
+    yc = get_yc(x, m, p)
+    theta = get_theta(x, m, p)
+    # x_upper, x_lower, y_upper, y_lower
+    return [x - yt*np.sin(theta), x + yt*np.sin(theta), yc + yt*np.cos(theta), yc - yt*np.cos(theta)]
+
+x = np.linspace(0.0, 1.0, 83)
+
+x_upper, x_lower, y_upper, y_lower = get_coords(x, m, p, t)
+print("x_upper =", x_upper)
+print("y_upper =", y_upper)
+print("x_lower =", x_lower)
+print("y_lower =", y_lower)
+
+constrained = True
+document = 'Unnamed'
+
+App.newDocument()
+App.activeDocument().addObject('PartDesign::Body','Body')
+App.ActiveDocument.getObject('Body').Label = 'Corpo'
+
+chord_hub = 340 # mm
+span = 1200 # mm
+n_span = 11
+spans = np.linspace(0.0, span, n_span) # mm
+chords = np.linspace(chord_hub, 0.2*chord_hub, n_span) # mm
+pitchs = np.linspace(0, 60, n_span) # degree
+delta_x = np.linspace(0, 0.1*chord_hub, n_span) # mm
+delta_y = np.linspace(0, 0.2*chord_hub, n_span) # mm
+
+sketch_names = []
+for span_id, span_height in enumerate(spans):
+    sketch = 'Sketch' + str(span_id)
+    sketch_names.append(sketch)
+
+    App.getDocument('Unnamed').getObject('Body').newObject('Sketcher::SketchObject',sketch)
+    App.getDocument('Unnamed').getObject(sketch).AttachmentSupport = (App.getDocument('Unnamed').getObject('XY_Plane'),[''])
+    App.getDocument('Unnamed').getObject(sketch).MapMode = 'FlatFace'
+    FreeCAD.getDocument('Unnamed').getObject(sketch).AttachmentOffset = App.Placement(App.Vector(delta_x[span_id],delta_y[span_id],span_height),App.Rotation(App.Vector(0,0,1),pitchs[span_id]))
+
+    x_upper_chord = x_upper*chords[span_id]
+    x_lower_chord = x_lower*chords[span_id]
+    y_upper_chord = y_upper*chords[span_id]
+    y_lower_chord = y_lower*chords[span_id]
+
+    vectors_upper = []
+    vectors_lower = []
+
+    points_upper = []
+    points_lower = []
+
+    object_id = -1
+    point_ids_upper = []
+    point_ids_lower = []
+
+    for i in range(len(x)):
+        # suction side (upper points)
+        object_id += 1
+        vectors_upper.append(App.Vector(x_upper_chord[i], y_upper_chord[i], 0))
+        points_upper.append(Part.Point(vectors_upper[i]))
+        point_ids_upper.append(object_id)
+        App.getDocument('Unnamed').getObject(sketch).addGeometry(points_upper[i], True) # the boolean flag is construction mode or not
+#        if constrained:
+#            App.getDocument('Unnamed').getObject(sketch).addConstraint(Sketcher.Constraint('DistanceX',point_ids_upper[i],1,x_upper_chord[i]))
+#            App.getDocument('Unnamed').getObject(sketch).addConstraint(Sketcher.Constraint('DistanceY',point_ids_upper[i],1,y_upper_chord[i]))
+
+        # pressure side (lower points)
+        object_id += 1
+        vectors_lower.append(App.Vector(x_lower_chord[i], y_lower_chord[i], 0))
+        points_lower.append(Part.Point(vectors_lower[i]))
+        point_ids_lower.append(object_id)
+        App.getDocument('Unnamed').getObject(sketch).addGeometry(points_lower[i], True) # the boolean flag is construction mode or not
+#        if constrained:
+#            App.getDocument('Unnamed').getObject(sketch).addConstraint(Sketcher.Constraint('DistanceX',point_ids_lower[i],1,x_lower_chord[i]))
+#            App.getDocument('Unnamed').getObject(sketch).addConstraint(Sketcher.Constraint('DistanceY',point_ids_lower[i],1,y_lower_chord[i]))
+
+
+    #sys.exit()
+    # end of chord (auxiliary point)
+    object_id += 1
+    chord_end_id = object_id
+
+    chord_end_vector = App.Vector(chords[span_id], 0, 0)
+    chord_end_point = Part.Point(chord_end_vector)
+
+    App.getDocument('Unnamed').getObject(sketch).addGeometry(chord_end_point, True)
+    if constrained:
+        App.getDocument('Unnamed').getObject(sketch).addConstraint(Sketcher.Constraint('DistanceX',chord_end_id,1,chords[span_id]))
+        #App.getDocument('Unnamed').getObject(sketch).addConstraint(Sketcher.Constraint('DistanceY',chord_end_id,1,0))
+        App.getDocument('Unnamed').getObject(sketch).addConstraint(Sketcher.Constraint('PointOnObject',chord_end_id,1,-1))
+
+
+    #sys.exit()
+    # suction side (upper spline)
+    object_id += 1
+    spline_upper_id = object_id
+
+    _finalbsp_poles = []
+    _finalbsp_knots = []
+    _finalbsp_mults = []
+
+    spline_upper = Part.BSplineCurve()
+    spline_upper.interpolate(vectors_upper, PeriodicFlag=False)
+    spline_upper.increaseDegree(3)
+
+    _finalbsp_poles.extend(spline_upper.getPoles())
+    _finalbsp_knots.extend(spline_upper.getKnots())
+    _finalbsp_mults.extend(spline_upper.getMultiplicities())
+
+    App.getDocument('Unnamed').getObject(sketch).addGeometry(Part.BSplineCurve(_finalbsp_poles,_finalbsp_mults,_finalbsp_knots,False,3,None,False),False)
+
+    del(_finalbsp_poles)
+    del(_finalbsp_knots)
+    del(_finalbsp_mults)
+
+    if constrained:
+        conList = []
+        for i in range(len(x)):
+            conList.append(Sketcher.Constraint('InternalAlignment:Sketcher::BSplineKnotPoint',point_ids_upper[i],1,spline_upper_id,i))
+        App.getDocument('Unnamed').getObject(sketch).addConstraint(conList)
+        del conList
+
+        App.getDocument('Unnamed').getObject(sketch).addConstraint(Sketcher.Constraint('Coincident', point_ids_upper[0], 1, -1, 1))
+        App.getDocument('Unnamed').getObject(sketch).addConstraint(Sketcher.Constraint('Block',spline_upper_id))
+
+
+    #sys.exit()
+    # pressure side (lower spline)
+    object_id += 1
+    spline_lower_id = object_id
+
+    _finalbsp_poles = []
+    _finalbsp_knots = []
+    _finalbsp_mults = []
+
+    spline_lower = Part.BSplineCurve()
+    spline_lower.interpolate(vectors_lower, PeriodicFlag=False)
+    spline_lower.increaseDegree(3)
+
+    _finalbsp_poles.extend(spline_lower.getPoles())
+    _finalbsp_knots.extend(spline_lower.getKnots())
+    _finalbsp_mults.extend(spline_lower.getMultiplicities())
+
+    App.getDocument('Unnamed').getObject(sketch).addGeometry(Part.BSplineCurve(_finalbsp_poles,_finalbsp_mults,_finalbsp_knots,False,3,None,False),False)
+
+    del(_finalbsp_poles)
+    del(_finalbsp_knots)
+    del(_finalbsp_mults)
+
+    if constrained:
+        conList = []
+        for i in range(len(x)):
+            conList.append(Sketcher.Constraint('InternalAlignment:Sketcher::BSplineKnotPoint',point_ids_lower[i],1,spline_lower_id,i))
+        App.getDocument('Unnamed').getObject(sketch).addConstraint(conList)
+        del conList
+
+        App.getDocument('Unnamed').getObject(sketch).addConstraint(Sketcher.Constraint('Coincident', point_ids_lower[0], 1, point_ids_upper[0], 1))
+        App.getDocument('Unnamed').getObject(sketch).addConstraint(Sketcher.Constraint('Block',spline_lower_id))
+
+
+    #sys.exit()
+    # trailing edge upper
+    object_id += 1
+    TE_upper_id = object_id
+    TE_upper_line = Part.LineSegment(vectors_upper[-1],chord_end_vector)
+    App.getDocument('Unnamed').getObject(sketch).addGeometry(TE_upper_line,False)
+
+    App.getDocument('Unnamed').getObject(sketch).addConstraint(Sketcher.Constraint('Coincident', TE_upper_id, 1, point_ids_upper[-1], 1))
+    App.getDocument('Unnamed').getObject(sketch).addConstraint(Sketcher.Constraint('Coincident', TE_upper_id, 2, chord_end_id, 1))
+
+
+    #sys.exit()
+    # trailing edge lower
+    object_id += 1
+    TE_lower_id = object_id
+    TE_lower_line = Part.LineSegment(vectors_lower[-1],chord_end_vector)
+    App.getDocument('Unnamed').getObject(sketch).addGeometry(TE_lower_line,False)
+
+    App.getDocument('Unnamed').getObject(sketch).addConstraint(Sketcher.Constraint('Coincident', TE_lower_id, 1, point_ids_lower[-1], 1))
+    App.getDocument('Unnamed').getObject(sketch).addConstraint(Sketcher.Constraint('Coincident', TE_lower_id, 2, chord_end_id, 1))
+
+
+    #sys.exit()
+    # end of loop
+
+#sys.exit()
+App.getDocument('Unnamed').getObject('Body').newObject('PartDesign::AdditiveLoft','AdditiveLoft')
+App.getDocument('Unnamed').getObject('AdditiveLoft').Profile = App.getDocument('Unnamed').getObject('Sketch0')
+App.getDocument('Unnamed').getObject('Sketch0').Visibility = False
+
+for i in range(1, n_span):
+    App.getDocument('Unnamed').getObject('AdditiveLoft').Sections += [(App.getDocument('Unnamed').getObject(sketch_names[i]), [''])] # sketch_names[1:]
+    App.getDocument('Unnamed').getObject(sketch_names[i]).Visibility = False
+
+# Show result
+App.ActiveDocument.recompute()
